f67b05495cae7b0aaabbd38db0f4ca6d7c57e32b
yangfk5 <yangfk5@midea.com>
2023-03-11 16:37:32 +0800

    init

---
+++ leaninghub/spark-core/src/main/java/com/frank/spark/core/worldcount/Spark01_WorldCount.scala
@@ -0,0 +1,46 @@
+package com.frank.spark.core.worldcount
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark01_WorldCount {
+
+
+  def main(args: Array[String]): Unit = {
+    //  application
+    // spark 框架
+
+    // 建立和 spark 框架的连接
+    // jdbc: Connection
+    val sparkConf = new SparkConf().setMaster("local").setAppName("WorldCount")
+    val sc = new SparkContext(sparkConf)
+
+    // 执行 业务操作
+    // 1. 读取文件，获取一行一行的数据
+    val lines: RDD[String] = sc.textFile("datas")
+    // 2. 将一行数据进行拆分，形成一个个的单词（分词）
+    // "hello world" => hello, world, hello, world
+    val words: RDD[String] = lines.flatMap(_.split(" "))
+    
+    // 3. 将数据根据单词进行分组，便于统计
+    // (hello, hello, hello), (world, world)
+    val wordGroup: RDD[(String, Iterable[String])] = words.groupBy(word=>word)
+
+    // 4. 对分组后的数据进行转换
+    // (hello, hello, hello), (world, world)
+    // (hello, 3) (world, 2)
+    val wordToCount: RDD[(String, Int)] = wordGroup.map{
+      case (word, list) =>
+        (word, list.size)
+    }
+
+    // 5. 将装换结果采集到控制台进行打印
+    val array: Array[(String, Int)] = wordToCount.collect();
+    array.foreach(println)
+
+
+    // 关闭连接
+    sc.stop()
+  }
+
+}
